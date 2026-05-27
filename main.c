361cb2ee33a58137926e76a9b595392919a42943
60262697 박승우 <happywithj@mju.ac.kr>
2026-05-27 09:17:14 +0000

    [박승우_60262697-V3.0제출-1]

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
 /* 
   파일이름: main.c
   작 성 자: 박승우
-  하 는 일: 야구 타자 성적 관리 시스템 v2.0
+  하 는 일: 야구 타자 성적 관리 시스템 v3.0
 */
 
 #include <stdio.h>
 
-int main()
-{   
-    char initial;               // 타자 이니셜
-    int game;                   // 경기 수
-    int at_bat;                 // 타수
-    int hit;                    // 안타 수
-    int home_run;               // 홈런 수
-    float last_avg;             // 지난 시즌 타율
-    float current_avg;          // 현재 타율
-    float avg_diff;             // 타율 차이
-    float f_hit;                // 실수 계산용 안타 수
-    float f_at_bat;             // 실수 계산용 타수
+/* ===== 전역 변수 선언 ===== */
+char initial;               // 타자 이니셜
+int game;                   // 경기 수
+int at_bat;                 // 타수
+int hit;                    // 안타 수
+int home_run;               // 홈런 수
+
+int is_input = 0;           // 입력 여부 확인
+int is_calculated = 0;      // 계산 여부 확인
+
+float last_avg;             // 지난 시즌 타율
+float current_avg;          // 현재 타율
+float avg_diff;             // 타율 차이
+
+/* ===== 함수 원형 선언 ===== */
 
     printf("================================================\n");
     printf("\t야구 타자 성적 관리 시스템 v2.0\n");
